1af7fe2efe8e5189b068f306f88cd2c31d05d850
naveenpaul143 <naveenpaul1433@gmail.com>
2025-06-23 23:13:54 +0530

    ck login flow ui test cases developed

--- src/main/java/com/CkTestCases/LoginFlow.java
+++ src/main/java/com/CkTestCases/LoginFlow.java
@@ -6,18 +6,14 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
-public class LaunchBrowser {
+public class LoginFlow {
 
     public void launchBrowser() {
         WebDriverManager.chromedriver().setup();
         WebDriver d = new ChromeDriver();
         d.get("https://cashkaro.com");
         WebElement logo = d.findElement(By.xpath("//img[@title='CashKaro']"));
-        if (logo.isDisplayed()) {
-            System.out.println("Successfully navigated to Cashkaro application");
-        } else {
-            System.out.println("Failed to navigated to Cashkaro application");
-        }
+         System.out.println("Login flow successfully modfied....");
 
     }
 }
